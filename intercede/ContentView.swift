75a5af0bca6dc4f90df01f41363c3bcc5233a5f4
parker7johnson <ptj7@uw.edu>
2026-02-04 16:11:58 -0800

    Initial Commit

---
+++ intercede/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  intercede
+//
+//  Created by Parker Johnson on 2/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ intercede/intercedeApp.swift
@@ -0,0 +1,17 @@
+//
+//  intercedeApp.swift
+//  intercede
+//
+//  Created by Parker Johnson on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct intercedeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
